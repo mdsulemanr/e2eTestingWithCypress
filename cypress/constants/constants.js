2398ed8033217f6a7b13b6d609c3009f2d2783aa
mdsulemanr <mdsulemanr@gmail.com>
2024-07-24 15:11:52 +0500

    'Separated all strings into constant file, then called into page and made use in our test file'

---
+++ cypress/constants/constants.js
@@ -0,0 +1,28 @@
+export const LOGIN_URL = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login';
+export const USERNAME_SELECTOR = "input[placeholder='Username']";
+export const PASSWORD_SELECTOR = "input[placeholder='Password']";
+export const SUBMIT_BUTTON_SELECTOR = "button[type='submit']";
+export const LOGO_SELECTOR = '.orangehrm-login-branding > img';
+export const LINKS_XPATH = '//a';
+export const USER_DROPDOWN_NAME_SELECTOR = ".oxd-userdropdown-name";
+export const LOGIN_BUTTON_SELECTOR = '.login-button';
+export const NAV_BAR_SELECTOR = '.nav-bar';
+export const WELCOME_MESSAGE_SELECTOR = '.welcome-message';
+export const LIST_ITEMS_SELECTOR = '.list-items';
+export const HEADER_SELECTOR = '.header';
+export const EXPECTED_URL = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login';
+export const EXPECTED_TEXTS = {
+  urlIncludes: 'orangehrmlive.com',
+  urlContains: 'orangehrm',
+  titleIncludes: 'Orange',
+  titleEquals: 'OrangeHRM',
+  titleContains: 'HRM',
+  welcomeMessage: 'Welcome',
+  expectedUserName: 'Ravi Arole'
+};
+export const LINK_COUNT = 5;
+export const HEADER_FONT_SIZE = '24px';
+export const CREDENTIALS = {
+  username: 'Admin',
+  password: 'admin123'
+};
