449bdbaa31fab6b431c79d2e90e81964d3d309b5
mdsulemanr <mdsulemanr@gmail.com>
2024-08-13 15:47:41 +0500

    Applied POM on mouseOperations.cy.js

--- cypress/e2e/fixtures.cy.js
+++ cypress/e2e/fixtures.cy.js
@@ -3,18 +3,18 @@
 describe('My TestSuite', () => {
 
     // Direct Access - if we want to use in a single block
-    // it('FixturesDemoTest', ()=>{
-    //     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    it('FixturesDemoTest', ()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
-    //     cy.fixture("orangehrm").then((data)=>{
-    //         cy.get("input[placeholder='Username']").type(data.username)
-    //         cy.get("input[placeholder='Password']").type(data.password)
-    //         cy.get("button[type='submit']").click()
-    //         cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should("have.text", data.expected)
+        cy.fixture("orangehrm").then((data)=>{
+            cy.get("input[placeholder='Username']").type(data.username)
+            cy.get("input[placeholder='Password']").type(data.password)
+            cy.get("button[type='submit']").click()
+            cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should("have.text", data.expected)
      
-    //     })
+        })
         
-    // })
+    })
 
     // Access through hook - for multiple it blocks
     let userdata;
